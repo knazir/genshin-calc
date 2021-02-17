efd31add09aef4e27ffc3a62dd7bde9b8be5986e
Kashif Nazir <kashif@cs.stanford.edu>
2021-02-16 18:33:20 -0800

    Really bad at GA stuff, here's another commit wheee

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
 
 import App from "./App";
 import "./index.css";
-
-// Analytics
-// try {
-//   ReactGA.initialize("G-SZJNJJP2JP");
-//   ReactGA.pageview(`${window.location.pathname}${window.location.search}`);
-// } catch { }
 
 ReactDOM.render(
   <App/>,
